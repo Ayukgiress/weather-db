4f029734ae9dedc7a8b285cf0a0a6865d9e078c7
Ayuk Giress <giress865@gmail.com>
2024-06-29 13:52:56 +0100

    finished backends

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ import cors from 'cors';
 
 import indexRouter from "./routes/index.js";
 const PORT = 5000;
-// import userRouter from "./routes/users.js"
 
 const app = express();
 
@@ -18,18 +17,14 @@ app.use(cors());
 
 app.use("/", indexRouter);
 
-// catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));
 });
 
-// error handler
 app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get("env") === "development" ? err : {};
 
-  // render the error page
   res.status(err.status || 500);
   res.send({ message: err.message, code: err.status });
 });
